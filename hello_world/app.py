e6fbbfe05c21d62e7387b52471082e3899041b20
shimabox <shimabox@gmail.com>
2022-11-20 16:59:24 +0900

    :+1: 5-6 別のバケットにファイルを書き出す

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,10 +1,38 @@
-import json
+import boto3
+import tempfile
+import os
 
-# import requests
+def lambda_handler (event, context):
+    # S3にアクセスするためのオブジェクトを取得
+    s3 = boto3.resource('s3')
+    tmpdir = tempfile.TemporaryDirectory()
+    for rec in event ['Records']:
+        filename = rec['s3']['object']['key']
+        bucketname = rec['s3']['bucket']['name']
 
+        # ファイルにアクセスするためのオブジェクトを取得
+        obj = s3.Object (bucketname, filename)
 
-def lambda_handler(event, context):
-    print(json.dumps(event, indent=2))
-    for rec in event['Records']:
-        print('file_name=' + rec['s3']['object']['key'])
-        print('bucket_name=' + rec['s3']['bucket']['name'])
+        # ファイルの情報を取得
+        response = obj.get()
+
+        # 一時ディレクトリにダウンロード
+        localfilename = os.path.join(tmpdir.name, filename)
+        fp = open(localfilename, 'wb')
+        fp.write(response ['Body'].read())
+        fp.close()
+
+        # もうひとつのパケットに書き込む
+        # 環境変数から書き出し先のバケット名を取
+        destbucketname = os.environ['OUTPUTBUCKET']
+
+        # ファイルにアクセスするためのオブジェクトを取得
+        obj2 = s3.Object(destbucketname, filename)
+
+        # アップロード
+        response = obj2.put(
+            Body = open(localfilename, 'rb')
+        )
+
+        # 一時ファイルのクリーンアップ
+        tmpdir.cleanup()
